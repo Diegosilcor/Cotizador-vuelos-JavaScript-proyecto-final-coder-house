bd673488f553ed95a0a28a148d176e5210dd4ef8
Diego Silva Cordoba <diegosilvacordoba@hotmail.com>
2022-05-27 12:24:06 +0200

    Desafio complementario clase 3. Comision 37455. Diego Silva Cordoba

--- script.js
+++ script.js
@@ -1,61 +1,28 @@
-// Prompt y concatenacion de texto
+// Clase 3 desafio complementario
 
-let nombre = prompt("Introduce tu nombre");
-let apellido = prompt("Introduce tu apellido");
-let edad = prompt("Introduce tu edad");
-let nacionalidad = prompt("Introduce tu nacionalidad");
-let residencia = prompt("Introduce tu residencia");
-
-
-alert("Hola Bienvenido " + nombre +  " "  + apellido + " al curso de javascript");
-
-
-
-//Prompt con suma
-
-// ParseInt para suma de numero enteros
-// ParseFloat para suma de numero decimales
-let numero1 = parseInt(prompt("Ingrese un numero"));
-let numero2 = parseInt(prompt("Ingrese otro numero"));
-let resultado = numero1 + numero2;
-alert("El resultado de la suma es " + resultado);
-
-
-// Concatenacion de numeros
-
-// Suma
-
-//let numero1 = 30;
-//let numero2 = 40;
-//let resultadoSuma = numero1 + numero2;
-//console.log(resultadoSuma);
-
-
-// Multiplicacion
-
-//let resultadoMultiplicacion = numero1 * numero2;
-//console.log(resultadoMultiplicacion);
-
-
-// Resta
-
-//let resultadoResta = numero1 - numero2;
-//console.log(resultadoResta);
-
-
-// Division
-
-//let resultadoDivision = numero1 / numero2;
-//console.log(resultadoDivision);
-
-
-
-
-
-
-
-
-
-
-
-
+let ingresarComida = prompt("¿Que deseas comer hoy?");
+let cantidad = parseInt(prompt ("¿Cuántos quieres comprar?"));
+while (ingresarComida != 'ESC') {
+    switch (ingresarComida) {
+        case "Hamburguesa":
+            alert("Tu pedido esta en proceso");
+            break;
+        case "Hamburguesa doble queso":
+            alert("Tu pedido esta en proceso");
+            break;
+        case "Pizza":
+            alert("Tu pedido esta en proceso");
+            break;
+        case "Hot dog":
+            alert("Tu pedido esta en proceso");
+            break;
+        case "Pollo frito":
+            alert("Tu pedido esta en proceso");
+            break;
+        default:
+            alert("Lo sentimos, no tenemos disponible ese menu");
+            break;
+    }
+    ingresarComida = prompt("¿Que deseas comer hoy?");
+    cantidad = parseInt(prompt ("¿Cuántos quieres comprar?"));
+}
