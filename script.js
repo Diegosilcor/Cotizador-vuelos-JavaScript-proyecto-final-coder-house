c3b83e24b0ea50b09e8b13a9dc91630b7d5b2ae2
Diego Silva Cordoba <diegosilvacordoba@hotmail.com>
2022-06-14 12:53:27 +0200

    Primera pre entrega de trabajo final 14.06.2022

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// PRIMERA PRE ENTREGA DE TRABAJO FINAL
+// PRIMERA PRE ENTREGA DE TRABAJO FINAL. JAVASCRIPT
+// Diego Ignacio Silva Cordoba
 
-//VARIABLES GLOBALES
+
+//VARIABLES 
 let america = 0;
 let europa = 0;
 let asia = 0;
@@ -14,7 +16,6 @@ let resultadoOceania = 0;
 let total = 0;
 
 //CONSTANTES
-
 const precioAmerica = 1000;
 const precioEuropa = 1200;
 const precioAsia = 2000;
@@ -25,111 +26,117 @@ const iva = 0.21;
 //FUNCION PARA SOLICITAR VUELOS AMERICA
 
 function solicitarAmerica() {
-    america = "America";
+    tipo = "pasajes";
     america = parseInt(prompt("Tarifas de vuelos para America\n\nPrecio unitario: " + precioAmerica + " PESOS\nDescuento del 10% para cantidades superiores a 1 pasaje\n\nIngrese la cantidad:"));
-
-    resultadoAmerica = calculos();
+    noEsNumero(america,tipo);
+    resultadoAmerica = calculos(america,tipo,precioAmerica,resultadoAmerica);
     return resultadoAmerica;
 }
 
 //FUNCION PARA SOLICITAR VUELOS EUROPA
 
 function solicitarEuropa() {
-    europa = "Europa";
+    tipo = "pasajes";
     europa = parseInt(prompt("Tarifas de vuelos para Europa\n\nPrecio unitario: " + precioEuropa + " PESOS\nDescuento del 10% para cantidades superiores a 1 pasaje\n\nIngrese la cantidad:"));
-
-    resultadoEuropa = calculos();
+    noEsNumero(europa,tipo);
+    resultadoEuropa = calculos(europa,tipo,precioEuropa,resultadoEuropa);
     return resultadoEuropa;
 }
 //FUNCION PARA SOLICITAR VUELOS ASIA
 
 function solicitarAsia() {
-    asia = "Asia";
+    tipo = "pasajes";
     asia = parseInt(prompt("Tarifas de vuelos para Asia\n\nPrecio unitario: " + precioAsia + " PESOS\nDescuento del 10% para cantidades superiores a 1 pasaje\n\nIngrese la cantidad:"));
-
-    resultadoAsia = calculos();
+    noEsNumero(asia,tipo);
+    resultadoAsia = calculos(asia,tipo,precioAsia,resultadoAsia);
     return resultadoAsia;
 }
-//FUNCION PARA SOLICITAR VUELOS A  OCEANIA
+//FUNCION PARA SOLICITAR VUELOS A OCEANIA
 function solicitarOceania() {
-    oceania = "Oceania";
+    tipo = "pasajes";
     oceania = parseInt(prompt("Tarifas de vuelos para Oceania\n\nPrecio unitario: " + precioOceania + " PESOS\nDescuento del 10% para cantidades superiores a 1 pasaje\n\nIngrese la cantidad:"));
-
-    resultadoOceania = calculos();
+    noEsNumero(oceania,tipo);
+    resultadoOceania = calculos(oceania,tipo,precioOceania,resultadoOceania);
     return resultadoOceania;
 }
 //OBJETO COTIZACION
-function Cotizacion(cantAmerica, cantEuropa, cantAsia, cantOceania) {
+function Cotizacion(cantAmerica, cantEuropa, cantAsia, cantOceania){
     this.cantAmerica = cantAmerica;
     this.cantEuropa = cantEuropa;
     this.cantAsia = cantAsia;
     this.cantOceania = cantOceania;
-    this.cotizar = cotizar;
-}
+
 
 //COMPONENTES DEL OBJETO COTIZACION
-this.composicion = function () {
-    console.log("\n\nMi cotización finalmente se compuso de: " +
-        "\n" + cantAmerica + " Vuelos America" +
-        "\n" + cantAsia + " Vuelos Asia" +
-        "\n" + cantEuropa + " Vuelos Europa" +
-        "\n" + cantOceania + " Vuelos Oceania");
-}
+    this.composicion = function(){
+        console.log("\n\nMi cotización finalmente se compuso de: " +
+            "\n" + cantAmerica + "pasajes" +
+            "\n" + cantAsia + "pasajes" +
+            "\n" + cantEuropa + "pasajes" +
+            "\n" + cantOceania + "pasajes");
+    }
+
 
 //CALCULAR EL VALOR DE LA COTIZACION
 
-this.cotizar = function () {
+this.cotizar = function() {
 
     total = resultadoAmerica + resultadoEuropa + resultadoAsia + resultadoOceania;
-    totalIva = total + (total * iva);
+    totalIva = total + (total*iva);
 
     console.log("\n\nCOTIZACIÓN FINAL\n\nVUELOS | Cantidad: " + cantAmerica + " / Subtotal: " + resultadoAmerica + " PESOS" +
-        "\nVUELOS EUROPA | Cantidad: " + cantEuropa + " / Subtotal: " + resultadoeEuropa + " PESOS" +
-        "\nCOMBOS DE VUELOS ASIA  | Cantidad: " + cantAsia + " / Subtotal: " + resultadoAsia + " PESOS" +
-        "\nCOMBOS DE VUELOS OCEANIA | Cantidad: " + cantOceania + " / Subtotal: " + resultadoOceania + " PESOS" +
+        "\nVUELOS EUROPA | Cantidad: " + cantEuropa + " / Subtotal: " + resultadoEuropa + " PESOS" +
+        "\nPAQUETES DE VUELOS ASIA  | Cantidad: " + cantAsia + " / Subtotal: " + resultadoAsia + " PESOS" +
+        "\nPAQUETES DE VUELOS OCEANIA | Cantidad: " + cantOceania + " / Subtotal: " + resultadoOceania + " PESOS" +
         "\n\nTOTAL: " + total + " PESOS" +
         "\nTOTAL + IVA (21%): " + totalIva + " PESOS" +
         "\n\nARREGLO RESUMEN DE CANTIDADES: " + arregloResumenCantidades());
-
     alert("COTIZACIÓN FINAL\n\nVUELOS | Cantidad: " + cantAmerica + " / Subtotal: " + resultadoAmerica + " PESOS" +
-        "\nCOMBOS DE VUELOS EUROPA | Cantidad: " + cantEuropa + " / Subtotal: " + resultadoEuropa + " PESOS" +
-        "\nCOMBOS DE VUELOS ASIA | Cantidad: " + cantAsia + " / Subtotal: " + resultadoAsia + " PESOS" +
-        "\nCOMBOS DE VUELOS OCEANIA| Cantidad: " + cantOceania + " / Subtotal: " + resultadoOceania + " PESOS" +
+        "\nPAQUETES DE VUELOS EUROPA | Cantidad: " + cantEuropa + " / Subtotal: " + resultadoEuropa + " PESOS" +
+        "\nPAQUETES DE VUELOS ASIA | Cantidad: " + cantAsia + " / Subtotal: " + resultadoAsia + " PESOS" +
+        "\nPAQUETES DE VUELOS OCEANIA| Cantidad: " + cantOceania + " / Subtotal: " + resultadoOceania + " PESOS" +
         "\n\nTOTAL: " + total + " PESOS" +
         "\nTOTAL + IVA (21%): " + totalIva + " PESOS" +
         "\n\nARREGLO RESUMEN DE CANTIDADES: " + arregloResumenCantidades());
 }
 
+} 
 
 //FUNCION QUE INSTANCIA AL OBJETO Y CALCULA EN ÉL LA COTIZACION FINAL
 
-function totalCotizacion() {
-    //INSTANCIA DEL OBJETO COTIZACION
-
-    var miCotizacion = new Cotizacion(america, europa, asia, oceania);
+function totalCotizacion(){
+    miCotizacion = new Cotizacion(america, europa, asia, oceania);
     miCotizacion.cotizar();
     miCotizacion.composicion();
 }
+
 //ARREGLO RESUMEN DE CANTIDADES SOLICITADAS
 function arregloResumenCantidades() {
-    var partesCotizacion = [];
-    partesCotizacion.push("America: " + america);
-    partesCotizacion.push("Europa: " + europa);
-    partesCotizacion.push("Asia: " + asia);
-    partesCotizacion.push("Oceania: " + oceania);
+    partesCotizacion = [];
+    partesCotizacion.push("Vuelos America: " + america);
+    partesCotizacion.push("Vuelos Europa: " + europa);
+    partesCotizacion.push("Vuelos Asia: " + asia);
+    partesCotizacion.push("Vuelos Oceania: " + oceania);
     partesCotizacion = partesCotizacion.join(" / ");
     return partesCotizacion;
 }
 
-function calculos(america, europa, asia, oceania) {
+//VALIDAR NUMERO
+function noEsNumero(numero, tipo){
+    if(isNaN(numero) || numero < 0){
+        alert("Debes ingresar una cantidad " + tipo + " válido\nHaz una nueva solicitud");
+    }
+}
+
+function calculos(cantidad,tipo,precio,resultado){
     if (cantidad == 1 || cantidad == 0) {
         resultado = cantidad * precio;
-        console.log("Cantidad de " + tipo + ": " + cantidad + " | Precio: " + resultado + " PESOS");
+        console.log("Cantidad de pasajes  " + tipo + ": " + cantidad + " | Precio: " + resultado + " PESOS");
         alert("Cantidad de " + tipo + ": " + cantidad + " | Precio: " + resultado + " PESOS");
     }
     if (cantidad > 1) {
         resultado = cantidad * (precio * descuento);
-        console.log("Cantidad de " + tipo + ": " + cantidad + " | Precio: " + resultado + " PESOS");
+        console.log("Cantidad de pasajes " + tipo + ": " + cantidad + " | Precio: " + resultado + " PESOS");
         alert("Cantidad de " + tipo + ": " + cantidad + " | Precio: " + resultado + " PESOS");
     }
     return resultado;
